74731b5dd1264777821cccd60bc0209680de39cf
Jim <jsmith774@gmail.com>
2020-08-25 16:07:08 -0400

    added auth

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
 app.use(express.json());
 app.use('/folders', folderRouter);
 app.use('/notes', noteRouter);
